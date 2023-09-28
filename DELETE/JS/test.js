97a7b2893cdeeb39a360b10354aff1172bac5359
Siebe De Gelas <siebe.degelas@student.arteveldehs.be>
2023-09-28 16:21:34 +0200

    yeeeey oefening 3 done

--- DELETE/JS/test.js
+++ DELETE/JS/test.js
@@ -1,1 +1,41 @@
-console.log("test");
+const books = [
+  {
+    title: "Het Achterhuis",
+    author: "Anne Frank",
+    alreadyRead: true,
+  },
+  {
+    title: "Harry Potter en de Steen der Wijzen",
+    author: "J.K. Rowling",
+    alreadyRead: false,
+  },
+  {
+    title: "De Hobbit",
+    author: "J.R.R. Tolkien",
+    alreadyRead: true,
+  },
+  {
+    title: "1984",
+    author: "George Orwell",
+    alreadyRead: false,
+  },
+];
+
+console.log(`
+=============================================================================
+|                               READING LIST                                |
+=============================================================================
+`);
+
+for (const index in books) {
+  const book = books[index];
+  if (book.alreadyRead === true) {
+    console.log(`Al gelezen`);
+  } else {
+    console.log(`NOG niet gelezen`);
+  }
+}
+
+console.log(`
+=============================================================================
+`);
