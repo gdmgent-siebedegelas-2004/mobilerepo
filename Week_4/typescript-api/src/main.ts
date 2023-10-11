677af5b77ebd3debbc7f09394b26bcaf1ef35aba
gdmgent-siebedegelas-2004 <siebe.degelas@student.arteveldehs.be>
2023-10-11 12:00:18 +0200

    randomUser wth boi

--- Week_4/typescript-api/src/main.ts
+++ Week_4/typescript-api/src/main.ts
@@ -1,1 +1,18 @@
 import "./style.css";
+
+type RandomUser = {
+  name: string;
+  first: string;
+  last: string;
+  phone: number;
+};
+
+type Result = {
+  results: RandomUser[];
+};
+
+async function randomUser() {
+  const response = await fetch("https://randomuser.me/api?results=10");
+  const users = await response.json();
+  console.log(users);
+}
