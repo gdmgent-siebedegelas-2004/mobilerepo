ed364e262fdd565bfa838cebbedaa22d224015d4
gdmgent-siebedegelas-2004 <siebe.degelas@student.arteveldehs.be>
2023-10-15 11:45:47 +0200

    api w4

--- Week_4/typescript-api/src/main.ts
+++ Week_4/typescript-api/src/main.ts
@@ -1,18 +1,59 @@
 import "./style.css";
 
+// type RandomUser = {
+//   name: string;
+//   first: string;
+//   last: string;
+//   phone: number;
+// };
+
+// type Result = {
+//   results: RandomUser[];
+// };
+
+// async function randomUser() {
+//   const response = await fetch("https://randomuser.me/api?results=10");
+//   const users = await response.json();
+//   console.log(users);
+// }
+
 type RandomUser = {
-  name: string;
-  first: string;
-  last: string;
-  phone: number;
+  name: {
+    first: string;
+    last: string;
+  };
+  phone: string;
 };
 
 type Result = {
   results: RandomUser[];
 };
 
-async function randomUser() {
-  const response = await fetch("https://randomuser.me/api?results=10");
-  const users = await response.json();
-  console.log(users);
+async function fetchRandomUsers() {
+  const response = await fetch("https://randomuser.me/api?results=99");
+  const data: Result = await response.json();
+  return data.results;
 }
+
+async function displayUsers() {
+  const userList = document.getElementById("user-list");
+  if (!userList) {
+    console.error("Could not find user list element");
+    return;
+  }
+  const users = await fetchRandomUsers();
+
+  users.forEach((user, index) => {
+    const userElement = document.createElement("div");
+    userElement.innerHTML = `
+      <p><strong>User ${index + 1}:</strong></p>
+      <p>First Name: ${user.name.first}</p>
+      <p>Last Name: ${user.name.last}</p>
+      <p>Phone: ${user.phone}</p>
+      <hr>
+    `;
+    userList.appendChild(userElement);
+  });
+}
+
+displayUsers();
