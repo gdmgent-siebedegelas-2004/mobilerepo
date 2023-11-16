18daa5bf3b469e03548c190c78f457b1594924df
gdmgent-siebedegelas-2004 <siebe.degelas@student.arteveldehs.be>
2023-11-16 18:00:22 +0100

    insomnia get done

--- Week_8.2/api/index.ts
+++ Week_8.2/api/index.ts
@@ -1,3 +1,26 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import * as bodyParser from "body-parser";
+import fs from "fs";
+import { Student } from "./types";
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const readStudents = (): Student[] => {
+  return JSON.parse(fs.readFileSync("data/students.json", "utf-8"));
+};
+
+const writeStudents = (students: Student[]) => {
+  fs.writeFileSync("data/students.json", JSON.stringify(students));
+};
+
+app.get("/students", (req: Request, res: Response) => {
+  const students: Student[] = readStudents();
+  res.json(students);
+});
+
+app.listen(3002, () => {
+  console.log("Server is running on port 3002");
+});
