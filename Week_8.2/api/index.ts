41bb2a2d7d76a67f373c0f224de8e1da6c241a51
gdmgent-siebedegelas-2004 <siebe.degelas@student.arteveldehs.be>
2023-11-18 14:21:23 +0100

    patch json

--- Week_8.2/api/index.ts
+++ Week_8.2/api/index.ts
@@ -21,6 +21,48 @@ app.get("/students", (req: Request, res: Response) => {
   res.json(students);
 });
 
+app.get("/students/:id", (req: Request, res: Response) => {
+  const students: Student[] = readStudents();
+  const id = req.params.id;
+  const student = students.find((s) => s.id === id);
+  if (student) {
+    res.json(student);
+  } else {
+    res.status(404).json({ error: "Student not found" });
+  }
+});
+
+app.post("/students", (req: Request, res: Response) => {
+  const student: Student = {
+    image: "https://arteveldehogeschool.instructure.com/images/messages/avatar-50.png",
+    ...req.body,
+  };
+
+  const students = [...readStudents(), student];
+  // save to JSON
+  writeStudents(students);
+
+  // return added student
+  res.json(student);
+});
+
+// PATCH
+app.patch("/students/:id", async (req: Request, res: Response) => {
+  const id = req.params.id;
+  const student: Student | undefined = readStudents().find((student) => student.id === id);
+
+  if (student) {
+    const updatedStudent = { ...student, ...req.body };
+    // update database
+    const students = readStudents().map((student) => (student.id === id ? updatedStudent : student));
+    writeStudents(students);
+    // return updated student
+    res.json(updatedStudent);
+  } else {
+    res.status(404).json({ error: "Student not found" });
+  }
+});
+
 app.listen(3002, () => {
   console.log("Server is running on port 3002");
 });
